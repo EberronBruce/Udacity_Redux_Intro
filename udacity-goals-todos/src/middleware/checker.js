68eb6242fea72a03c5439a963c6aab73880c2a07
Bruce <bruce@redravencomputing.com>
2021-04-16 02:04:08 -0600

    Debug and added finalize folder structure

--- udacity-goals-todos/src/middleware/checker.js
+++ udacity-goals-todos/src/middleware/checker.js
@@ -3,14 +3,14 @@ import { ADD_GOAL } from '../actions/goals'
 
 const checker = (store) => (next) => (action) => {
   if (
-    action.type == ADD_TODO &&
+    action.type === ADD_TODO &&
     action.todo.name.toLowerCase().includes('bitcoin')
   ) {
     return alert("Nope. That's a bad idea.")
   }
 
   if (
-    action.type == ADD_GOAL &&
+    action.type === ADD_GOAL &&
     action.goal.name.toLowerCase().includes('bitcoin')
     ) {
     return alert("Nope. That's a bad idea.")
